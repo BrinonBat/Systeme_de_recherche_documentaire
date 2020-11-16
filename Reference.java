d4d01b3e37ad6b63f443abdc6d8382e61f3abc63
etudiant <etudiant@d2L2>
2020-11-16 16:57:56 +0100

    Fin de la classe " reference " gerant les poids de la matrice d'indexation

--- Reference.java
+++ Reference.java
@@ -1,16 +1,37 @@
 import java.util.Vector;
+import java.lang.Math;
 
 public class Reference {
     private float poids;
+    private float tf;
     private Vector<Integer> liste_positions;
 
-    //constructeur 
-    Reference(int nouv_poids,int position){
-        poids=nouv_poids;
+    //constructeur
+    Reference(){
         liste_positions= new Vector<Integer>();
-        liste_positions.add(position);
     }
 
     //accesseurs
+    public float getPoids(){
+        return this.poids;
+    }
+    public void setPoids(double idf){
+        this.poids=(float)((1+Math.log10(tf))*(Math.log10(idf)));
+    }
 
+    public void majTf(int nb_mots){
+        this.tf=(liste_positions.size()/nb_mots);
+    }
+
+
+    /// Ajoute une occurence du mot à la liste d'occurences
+    public void ajoutOccurence(int position, int nb_mots){
+        //ajout de l'occurence dans la liste des positions
+        liste_positions.add(position);
+
+        //!\\ on ne fait pas la maj du TF à chaque ajout, car on a besoin du nombre total de mots du fichier pour l'avoir. 
+        //!\\ On le calcule donc une fois l'analyse du fichier fini
+    }
 }
+
+
