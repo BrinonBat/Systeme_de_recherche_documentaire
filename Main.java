ea02ac145b1b3fada8c18b8498b90dec4ea13788
etudiant <etudiant@d2L2>
2020-11-16 09:25:16 +0100

    mise de commentaires dans le main pour s'y retrouver

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 
+public class Main {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
+        //indexation des sources
+        //lancement de l'interface
+            // interface lance traitement de requête
+            // affiche le résultat
+    }
 
-    return;
 }
