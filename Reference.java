9acfc9442f555f6378ff017da67d214989667108
BrinonBat <baptiste@brinon.eu>
2020-11-18 19:19:00 +0100

    retrait du vecteur car surcharge du csv sinon (complexité en 0(N^2)

--- Reference.java
+++ Reference.java
@@ -4,11 +4,11 @@ import java.lang.Math;
 public class Reference {
     private float poids;
     private float tf;
-    private Vector<Integer> liste_positions;
-
+    private int quantite;
     //constructeur
     Reference(){
-        liste_positions= new Vector<Integer>();
+        poids=0;
+        quantite=0;
     }
 
     //accesseurs
@@ -20,18 +20,13 @@ public class Reference {
     }
 
     public void majTf(int nb_mots){
-        this.tf=(liste_positions.size()/nb_mots);
+        this.tf=(quantite/nb_mots);
     }
 
-
-    /// Ajoute une occurence du mot à la liste d'occurences
-    public void ajoutOccurence(int position, int nb_mots){
-        //ajout de l'occurence dans la liste des positions
-        liste_positions.add(position);
 
         //!\\ on ne fait pas la maj du TF à chaque ajout, car on a besoin du nombre total de mots du fichier pour l'avoir. 
         //!\\ On le calcule donc une fois l'analyse du fichier fini
-    }
+    
 }
 
 
