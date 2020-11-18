0a752c3a0e7caab7c41de1e24952c0948d670bb7
BrinonBat <baptiste@brinon.eu>
2020-11-18 18:56:03 +0100

    ajout d'un début d'automatisation de la gestion de la sauvegarde d'index

--- Indexer.java
+++ Indexer.java
@@ -2,24 +2,24 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Indexer {
+    public Reference[][] index;
     private File sources;
     private ArrayList<File> li_fics;
-
+    File fichier_index= new File("index.csv");
     //constructeur
     public Indexer(String chemin){
         sources = new File(chemin);
         li_fics=new ArrayList<File>(); 
-
+        if(!remplirIndex()) System.out.println("erreur lors de la récupération de l'index");
     }
 
     public void supprime_index(){
         li_fics.clear(); // on vide la liste des fichiers traités
 
         // on supprime l'index
-        File index= new File("index.csv");
-        if(index.exists()){
+        if(fichier_index.exists()){
             System.out.println("index correctement supprimé !");
-            index.delete();
+            fichier_index.delete();
         } else {
             System.out.println("aucun index à supprimer.");
         }
@@ -38,12 +38,14 @@ public class Indexer {
             }
         }
 
-
-
+        //s'il y a des fichiers à traiter, on doit re-construire la matrice d'indexation
 
         // on traite les fichiers qui sont à traiter jusqu'à ce qu'il n'y en ai plus.
         for(File fichier : aTraiter){
-            
+            // ajout fichier en colonne
+            // parcours du fichier 
+                //si mot inconnu, ajout en bas de la liste.
+                //si mot connu, augmente le nombre d'occurence 
         }
         File index= new File("index.csv");
     // tokenization
@@ -52,7 +54,19 @@ public class Indexer {
     //private 
 
     }
-    
-    
+    // génére l'index à partir du fichier csv. Retourne false s'il y a eu une erreur
+    private boolean remplirIndex(){
+        boolean isOk=false;
+
+        isOk=true;
+        return isOk;
+    }    
+
+    private boolean sauvIndex(){
+        boolean isOk=false;
+
+        isOk=true;
+        return isOk;
+    }
 
 }
