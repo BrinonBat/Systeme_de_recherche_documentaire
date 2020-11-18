c303c13b2526226852f3d5e3d18baf536edfe2cf
BrinonBat <baptiste@brinon.eu>
2020-11-18 17:18:18 +0100

    ajout d'une méthode permettant de réinitialiser l'index

--- Indexer.java
+++ Indexer.java
@@ -1,6 +1,43 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class Indexer {
-    private String path_to_src;
+    private File sources;
+    private ArrayList<File> li_fics;
+
+    //constructeur
+    public Indexer(String chemin){
+        sources = new File(chemin);
+        li_fics=new ArrayList<File>(); 
+
+    }
+
+    public void supprime_index(){
+        li_fics.clear(); // on vide la liste des fichiers traités
+
+        // on supprime l'index
+        File index= new File("index.csv");
+        if(index.exists()){
+            System.out.println("index correctement supprimé !");
+            index.delete();
+        } else {
+            System.out.println("aucun index à supprimer.");
+        }
+    }
+
+    public void indexation(){
+        File index= new File("index.csv");
+        //verification qu'au moins un fichier à été ajouté
+        ArrayList<File> aTraiter=sources.list();
+
+        //prends un fichier
+    // tokenization
+    // Stemmer
+    // indexation & stopwords
     //private 
+
+    }
+    
     
 
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         //indexation des sources AP si nécessaire
+        Indexer indexer= new Indexer("Assets/AP");
+        indexer.supprime_index();
             // compare les fichiers source et les colonnes de l'index, s'il en manque une lance l'indexation
 
         //lancement de l'interface
